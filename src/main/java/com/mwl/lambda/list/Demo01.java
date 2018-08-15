261545a1ce638fde0b5d744e10700d62d6acd685
mawenlong <15061886113@163.com>
2018-08-15 22:59:46 +0800

    函数式编程

---
+++ src/main/java/com/mwl/lambda/list/Demo01.java
@@ -0,0 +1,49 @@
+package com.mwl.lambda.list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author mawenlong
+ * @date 2018/08/15
+ * describe:
+ */
+public class Demo01 {
+    final List<String> friends =
+            Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+    public void printALl(){
+        //friends.forEach((final String name)->System.out.println(name));
+        //friends.forEach((name) -> System.out.println(name));
+        friends.forEach(System.out::println);
+    }
+    public void uppercaseNames(){
+        final List<String> uppercaseNames = new ArrayList<String>();
+        //1.
+        System.out.println("1.方式：");
+        friends.forEach(name -> uppercaseNames.add(name.toUpperCase()));
+        uppercaseNames.forEach(System.out::println);
+        //2.
+        System.out.println("2.方式：");
+        friends.stream()
+                .map(name->name.toUpperCase())
+                .forEach(name -> System.out.print(name + " "));
+        System.out.println("\n3.计算每个名字的字母个数：");
+        friends.stream()
+                .map(name -> name.length())
+                .forEach(count -> System.out.print(count + " "));
+
+        System.out.println("\n4.使用方法引用：");
+        friends.stream()
+                .map(String::toUpperCase)
+                .forEach(name -> System.out.println(name));
+
+    }
+
+    public static void main(String[] args) {
+        new Demo01().uppercaseNames();
+    }
+
+
+
+}
