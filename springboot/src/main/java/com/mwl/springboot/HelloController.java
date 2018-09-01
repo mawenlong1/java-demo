4356e642dc8fe38704640489999dce7c9d283ed0
mawenlong <15061886113@163.com>
2018-09-01 23:00:33 +0800

    SpringBoot Demo

--- springboot/src/main/java/com/mwl/springboot/HelloController.java
+++ springboot/src/main/java/com/mwl/springboot/HelloController.java
@@ -1,18 +1,18 @@
-package com.mwl.demo;
+package com.mwl.springboot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author mawenlong
- * @date 2018/08/26
+ * @date 2018/09/01
  * describe:
  */
 @RestController
-public class HelloWorldController {
-    @RequestMapping("/info")
-    public String info(){
-        return "HelloWorld";
-    }
+public class HelloController {
 
+    @RequestMapping("hello")
+    public String index(){
+        return "Hello world";
+    }
 }
