2ed7b36fe5d9429c53c999acf33876316a9ee599
mawenlong <15061886113@163.com>
2018-12-29 19:05:33 +0800

    日期格式化

---
+++ demo/src/test/java/QueryPrizeGoodsListSendParam.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+/**
+ * @author mawenlong
+ * @date 2018/12/29
+ */
+public class QueryPrizeGoodsListSendParam {
+    private Date tran_time;
+
+    public Date getTran_time() {
+        return tran_time;
+    }
+
+    public void setTran_time(Date tran_time) {
+        this.tran_time = tran_time;
+    }
+}
---
+++ demo/src/test/java/TestJson.java
@@ -0,0 +1,36 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author mawenlong
+ * @date 2018/12/29
+ */
+public class TestJson {
+  private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+  private static SimpleDateFormat format12 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+  private static SimpleDateFormat format24 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+  public static void main(String[] args) throws Exception {
+    QueryPrizeGoodsListSendParam param = new QueryPrizeGoodsListSendParam();
+    String s = "2018-12-27 13:10:37";
+    Date date = simpleDateFormat.parse(s);
+    System.out.println(date);
+    param.setTran_time(date);
+    System.out.println(gsonFactory().toJson(param));
+
+    System.out.println(format12.format(date));
+    System.out.println(format24.format(date));
+
+  }
+
+  public static Gson gsonFactory() {
+    GsonBuilder gsb = new GsonBuilder();
+    gsb.setDateFormat("yyyy-MM-dd hh:mm:ss");
+    Gson gs = gsb.create();
+    return gs;
+  }
+}
