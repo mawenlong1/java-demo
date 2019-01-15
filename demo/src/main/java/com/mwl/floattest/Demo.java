f6f215d0b5305ad4f155d0e3b05b14b05fb6e347
mawenlong <15061886113@163.com>
2019-01-15 16:54:52 +0800

    位运算相关

---
+++ demo/src/main/java/com/mwl/floattest/Demo.java
@@ -0,0 +1,30 @@
+package com.mwl.floattest;
+
+/**
+ * @author mawenlong
+ * @date 2019/01/15
+ */
+public class Demo {
+    private static final int NUM = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        float f = 1 << 24;
+        System.out.println(f == f + 1);
+        int sum = 0;
+        Long start = System.nanoTime();
+        for (int i = 0; i < NUM; i++) {
+            int temp = i & 0x3f;
+            sum += temp;
+            temp = sum & 0x3f;
+        }
+        System.out.println("位运算时间：" + (System.nanoTime() - start));
+        start = System.nanoTime();
+        for (int i = 0; i < NUM; i++) {
+            int temp = i % 64;
+            sum += temp;
+            temp = sum % 64;
+        }
+        System.out.println(" 取余时间：" + (System.nanoTime() - start));
+    }
+}
+
