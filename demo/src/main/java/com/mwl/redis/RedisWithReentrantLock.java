10d1805eb67b2bfbdf8c560e2214e94e7ec4f856
mawenlong <15061886113@163.com>
2019-02-21 22:39:15 +0800

    redis分布式锁

---
+++ demo/src/main/java/com/mwl/redis/RedisWithReentrantLock.java
@@ -0,0 +1,80 @@
+package com.mwl.redis;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author mawenlong
+ * @date 2019/02/21
+ * <p>
+ * redis 重入锁
+ */
+public class RedisWithReentrantLock {
+    private ThreadLocal<Map<String, Integer>> lockers = new ThreadLocal<>();
+
+    private Jedis jedis;
+
+    public RedisWithReentrantLock(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    private void doUnlock(String key) {
+        jedis.del(key);
+    }
+
+    private boolean doLock(String key) {
+        return jedis.set(key, "", "nx", "ex", 5L) != null;
+    }
+
+    private Map<String, Integer> currentLockers() {
+        Map<String, Integer> refs = lockers.get();
+        if (refs != null) {
+            return refs;
+        }
+        lockers.set(new HashMap<>());
+        return refs;
+    }
+
+    public boolean lock(String key) {
+        Map<String, Integer> refs = currentLockers();
+        Integer refCnt = refs.get(key);
+        if (refCnt != null) {
+            refs.put(key, refCnt + 1);
+            return true;
+        }
+        boolean ok = doLock(key);
+        if (!ok) {
+            return false;
+        }
+        refs.put(key, 1);
+        return true;
+    }
+
+    public boolean unlock(String key) {
+        Map<String, Integer> refs = currentLockers();
+        Integer refCnt = refs.get(key);
+        if (refCnt == null) {
+            return false
+                    ;
+        }
+        refCnt -= 1;
+        if (refCnt > 0) {
+            refs.put(key, refCnt);
+        } else {
+            refs.remove(key);
+            doUnlock(key);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("111.231.228.108",6379);
+        RedisWithReentrantLock redis = new RedisWithReentrantLock(jedis);
+        System.out.println(redis.lock("codehole"));
+        System.out.println(redis.lock("codehole"));
+        System.out.println(redis.unlock("codehole"));
+        System.out.println(redis.unlock("codehole"));
+    }
+}
