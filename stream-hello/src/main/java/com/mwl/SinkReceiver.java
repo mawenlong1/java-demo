b592e1ac735ae65c9009262759aade064409b422
mawenlong <15061886113@163.com>
2019-02-27 19:53:39 +0800

    stream  rabbit

---
+++ stream-hello/src/main/java/com/mwl/SinkReceiver.java
@@ -0,0 +1,12 @@
+package com.mwl;
+
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+/**
+ * @author mawenlong
+ * @date 2019/02/27
+ */
+@EnableBinding
+public class SinkReceiver {
+}
