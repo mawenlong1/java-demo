e3f4f3491c2e7142e8096431ace3feeb9ae335ab
mawenlong <mwljndx@163.com>
2019-06-16 19:56:03 +0800

    fix

--- springMVC/src/main/java/com/mwl/beanwrapper/Test.java
+++ springMVC/src/main/java/com/mwl/beanwrapper/Test.java
@@ -3,15 +3,13 @@ package com.mwl.beanwrapper;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.PropertyAccessorFactory;
 
-import javax.jws.soap.SOAPBinding.Use;
-
 public class Test {
     public static void main(String[] args) {
         User user = new User();
         BeanWrapper beanWrapper = PropertyAccessorFactory.forBeanPropertyAccess(user);
         beanWrapper.setPropertyValue("name","张三");
-        System.out.println(user);
+        System.out.println("前:"+user);
         beanWrapper.setPropertyValue("name","李四");
-        System.out.println(user);
+        System.out.println("后:"+user);
     }
 }
