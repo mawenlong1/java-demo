9c9fa62dd03f13fafd537c95f37aa30bf17bc031
mawenlong <mwljndx@163.com>
2019-06-17 13:56:26 +0800

    fix

--- springMVC/src/main/java/com/mwl/bridge/Main.java
+++ springMVC/src/main/java/com/mwl/bridge/Main.java
@@ -5,11 +5,11 @@ import java.lang.reflect.Method;
 public class Main {
     public static void main(String[] args) {
         C c = new D();
-        //System.out.println(D.class.getMethods());
+        System.out.println(D.class.getMethods());
         try {
             c.id(new Object());
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
 
         for (Method method : D.class.getMethods()) {
