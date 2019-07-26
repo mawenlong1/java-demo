39162ec8b28de31c7162ffec40d2648a20cb2612
mawenlong <mwljndx@163.com>
2019-07-26 17:42:41 +0800

    fix

--- springboot/src/main/java/com/mwl/guava/string/CaseFormatTest.java
+++ springboot/src/main/java/com/mwl/guava/string/CaseFormatTest.java
@@ -9,8 +9,8 @@ import com.google.common.base.CaseFormat;
 public class CaseFormatTest {
   public static void main(String[] args) {
     String data = "test_data";
-    System.out.println(CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, "test-data"));
-    System.out.println(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "test_data"));
-    System.out.println(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, "test_data"));
+    System.out.println(CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, data));
+    System.out.println(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, data));
+    System.out.println(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, data));
   }
 }
