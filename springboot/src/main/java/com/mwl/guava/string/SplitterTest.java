94c1975309ab51639364781d1ec3db99d3099e62
mawenlong <mwljndx@163.com>
2019-07-31 18:42:36 +0800

    fix

--- springboot/src/main/java/com/mwl/guava/string/SplitterTest.java
+++ springboot/src/main/java/com/mwl/guava/string/SplitterTest.java
@@ -7,8 +7,8 @@ import com.google.common.base.Splitter;
  * @date 2018/11/26
  */
 public class SplitterTest {
-  public static void main(String[] args) {
-    System.out.println(Splitter.on(',').trimResults().omitEmptyStrings().split(
-        "the ,quick, , brown         , fox,              jumps, over, the, lazy, little dog."));
-  }
+    public static void main(String[] args) {
+        System.out.println(Splitter.on(',').trimResults().omitEmptyStrings().split(
+                "the ,quick, , brown         , fox,              jumps, over, the, lazy, little dog."));
+    }
 }
