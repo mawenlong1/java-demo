1cf02674e788683383c849bcae2a085eae7017e3
mawenlong <mwljndx@163.com>
2019-08-01 20:33:31 +0800

    fix

--- springboot/src/main/java/com/mwl/guava/NullTest.java
+++ springboot/src/main/java/com/mwl/guava/NullTest.java
@@ -7,20 +7,20 @@ import com.google.common.base.Optional;
  * @date 2018/11/26
  */
 public class NullTest {
-  public static void main(String[] args) {
-    NullTest guavaTester = new NullTest();
-    Integer invalid = null;
-    Optional<Integer> a = Optional.fromNullable(invalid);
-    Optional<Integer> b = Optional.of(new Integer(10));
-    System.out.println("====="+guavaTester.sum(a, b));
-  }
-
-  public Integer sum(Optional<Integer> a, Optional<Integer> b) {
-    if( a.isPresent()==false){
-      a= a.of(new Integer(10));
+    public static void main(String[] args) {
+        NullTest guavaTester = new NullTest();
+        Integer invalid = null;
+        Optional<Integer> a = Optional.fromNullable(invalid);
+        Optional<Integer> b = Optional.of(new Integer(10));
+        System.out.println("=====" + guavaTester.sum(a, b));
     }
-    System.out.println("First parameter is present: " + a.isPresent());
-    System.out.println("Second parameter is present: " + b.isPresent());
-    return a.get() + b.get();
-  }
+
+    public Integer sum(Optional<Integer> a, Optional<Integer> b) {
+        if (a.isPresent() == false) {
+            a = a.of(new Integer(10));
+        }
+        System.out.println("First parameter is present: " + a.isPresent());
+        System.out.println("Second parameter is present: " + b.isPresent());
+        return a.get() + b.get();
+    }
 }
--- springboot/src/main/java/com/mwl/guava/OrderingTest.java
+++ springboot/src/main/java/com/mwl/guava/OrderingTest.java
@@ -8,21 +8,21 @@ import com.google.common.collect.Ordering;
  * @date 2018/11/26
  */
 public class OrderingTest {
-  public static void main(String[] args) {
-    testStaticOrdering();
-  }
+    public static void main(String[] args) {
+        testStaticOrdering();
+    }
 
-  public static void testStaticOrdering() {
-    ImmutableList<String> list = ImmutableList.of("peida", "jerry", "harry", "harry", "jhon",
-        "neron");
-    System.out.println(list);
+    public static void testStaticOrdering() {
+        ImmutableList<String> list = ImmutableList.of("peida", "jerry", "harry", "harry", "jhon",
+                                                      "neron");
+        System.out.println(list);
 
-    Ordering<String> naturalOrdering = Ordering.natural();
-    Ordering<Object> usingToStringOrdering = Ordering.usingToString();
-    Ordering<Object> arbitraryOrdering = Ordering.arbitrary();
+        Ordering<String> naturalOrdering = Ordering.natural();
+        Ordering<Object> usingToStringOrdering = Ordering.usingToString();
+        Ordering<Object> arbitraryOrdering = Ordering.arbitrary();
 
-    System.out.println("naturalOrdering:" + naturalOrdering.sortedCopy(list));
-    System.out.println("usingToStringOrdering:" + usingToStringOrdering.sortedCopy(list));
-    System.out.println("arbitraryOrdering:" + arbitraryOrdering.sortedCopy(list));
-  }
+        System.out.println("naturalOrdering:" + naturalOrdering.sortedCopy(list));
+        System.out.println("usingToStringOrdering:" + usingToStringOrdering.sortedCopy(list));
+        System.out.println("arbitraryOrdering:" + arbitraryOrdering.sortedCopy(list));
+    }
 }
