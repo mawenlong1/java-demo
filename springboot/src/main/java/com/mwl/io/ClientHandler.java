da9b67591659b4242c7d22ffce9b631fe9fc50c4
mawenlong <mwljndx@163.com>
2019-08-04 18:04:08 +0800

    fix

--- springboot/src/main/java/com/mwl/io/ClientHandler.java
+++ springboot/src/main/java/com/mwl/io/ClientHandler.java
@@ -11,23 +11,22 @@ import java.nio.charset.Charset;
  * @author mawenlong
  * @date 2018/11/28
  */
-public class ClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+public class ClientHandler extends SimpleChannelInboundHandler<ByteBuf>{
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
         byte[] buffer = new byte[msg.readableBytes()];
         msg.readBytes(buffer);
         String message = new String(buffer, Charset.forName("utf-8"));
-        System.out.println("客户端接收到的消息内容：" + message);
+        System.out.println("客户端接收到的消息内容："+message);
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        for (int i = 0; i < 10; i++) {
-            ByteBuf buffer = Unpooled.copiedBuffer("hello", Charset.forName("utf-8"));
+        for(int i = 0;i <10;i++){
+            ByteBuf buffer  = Unpooled.copiedBuffer("hello", Charset.forName("utf-8"));
             ctx.writeAndFlush(buffer);
         }
     }
-
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         super.exceptionCaught(ctx, cause);
