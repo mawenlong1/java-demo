514509f6042e13192fdcf8bb7c8994c9f5626b6f
mawenlong <mwljndx@163.com>
2019-09-11 19:37:46 +0800

    fix

--- springMVC/src/main/java/com/mwl/controller/DiguaHandlerMapping.java
+++ springMVC/src/main/java/com/mwl/controller/DiguaHandlerMapping.java
@@ -4,7 +4,6 @@ import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.HandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Calendar;
 
 /**
  * @author mawenlong
@@ -17,8 +16,11 @@ public class DiguaHandlerMapping implements HandlerMapping {
         String method = request.getMethod();
         if (url.startsWith("/tudou")) {
             if (method.equalsIgnoreCase("GET")) {
+                System.out.println("get 方法");
             } else if (method.equalsIgnoreCase("POST")) {
+                System.out.println("post 方法");
             } else {
+                //TODO
             }
         }
         return null;
