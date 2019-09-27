ea2fe606cb9ad3862c5047756b6cc59c9aecb7c5
mawenlong <mwljndx@163.com>
2019-09-27 16:07:06 +0800

    json时间序列化

--- demo/src/test/java/TestJson.java
+++ demo/src/test/java/TestJson.java
@@ -9,10 +9,10 @@ import java.util.Date;
  * @date 2018/12/29
  */
 public class TestJson {
-  private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+  private static  final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
-  private static SimpleDateFormat format12 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-  private static SimpleDateFormat format24 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+  private static final SimpleDateFormat format12 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+  private static final SimpleDateFormat format24 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
   public static void main(String[] args) throws Exception {
     QueryPrizeGoodsListSendParam param = new QueryPrizeGoodsListSendParam();
