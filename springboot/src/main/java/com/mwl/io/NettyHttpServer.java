7ba6c53179701be614722328d2cc91682a70f28f
mawenlong <mwljndx@163.com>
2019-10-06 20:48:52 +0800

    netty

--- springboot/src/main/java/com/mwl/io/NettyHttpServer.java
+++ springboot/src/main/java/com/mwl/io/NettyHttpServer.java
@@ -3,6 +3,7 @@ package com.mwl.io;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -18,7 +19,7 @@ public class NettyHttpServer {
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         ServerBootstrap bootstrap = new ServerBootstrap();
         bootstrap.group(bossGroup, workerGroup)
-//                 .option(ChannelOption.SO_BACKLOG, 1024)
+                 .option(ChannelOption.SO_BACKLOG, 1024)
                  .channel(NioServerSocketChannel.class)
                  .childHandler(new ChannelInitializer<SocketChannel>() {
                      @Override
