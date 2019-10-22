f24f8d8e45da3b4cc53985bc6dc088faf556ec65
mawenlong <mwljndx@163.com>
2019-10-22 20:39:05 +0800

    fix

--- demo/src/main/java/com/mwl/dateformat/DateTimeFormatterTest.java
+++ demo/src/main/java/com/mwl/dateformat/DateTimeFormatterTest.java
@@ -16,6 +16,7 @@ public class DateTimeFormatterTest {
         String dateStr = "2016年10月25日";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
         LocalDate date = LocalDate.parse(dateStr, formatter);
+        System.out.println(date);
 
         //日期转换为字符串
         LocalDateTime now = LocalDateTime.now();
