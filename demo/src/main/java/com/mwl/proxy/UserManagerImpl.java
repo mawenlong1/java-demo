52b8efac8ad8ed33d2e14fed7c2f6a01cfc8a0b2
mawenlong <mwljndx@163.com>
2019-11-05 17:54:25 +0800

    fix

--- demo/src/main/java/com/mwl/proxy/UserManagerImpl.java
+++ demo/src/main/java/com/mwl/proxy/UserManagerImpl.java
@@ -8,7 +8,7 @@ public class UserManagerImpl implements UserManager {
     @Override
     public void addUser(String userName, String password) {
         System.out.println("调用了新增的方法！");
-        System.out.println("传入参数为 userName: " + userName + " password: " + password);
+        System.out.println("传入参数为 userName: " + userName + ",password: " + password);
     }
 
     @Override
