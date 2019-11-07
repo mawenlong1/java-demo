7b9be963917dfc12918f9026906e44220550d6d1
mawenlong <mwljndx@163.com>
2019-11-07 14:29:16 +0800

    fix

--- demo/src/main/java/com/mwl/spi/Main.java
+++ demo/src/main/java/com/mwl/spi/Main.java
@@ -8,15 +8,15 @@ import java.util.ServiceLoader;
  * @date 2019/03/26
  */
 public class Main {
-  public static void main(String[] args) {
-    //内部使用当前线程ContextClassLoader,取去加载类
-    ServiceLoader serviceLoader = ServiceLoader.load(IRepository.class);
-    Iterator it = serviceLoader.iterator();
-    while (it != null && it.hasNext()) {
-      IRepository demoService = (IRepository) it.next();
-      System.out.println("class:" + demoService.getClass().getName());
-      demoService.save("tom");
-    }
+    public static void main(String[] args) {
+        //内部使用当前线程ContextClassLoader,取去加载类
+        ServiceLoader serviceLoader = ServiceLoader.load(IRepository.class);
+        Iterator it = serviceLoader.iterator();
+        while (it != null && it.hasNext()) {
+            IRepository demoService = (IRepository) it.next();
+            System.out.println("class:" + demoService.getClass().getName());
+            demoService.save("tom");
+        }
 
-  }
+    }
 }
