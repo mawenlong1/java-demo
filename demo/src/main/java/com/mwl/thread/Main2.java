9ef6a30d805dd3da332047b7ffac795237ccf9ae
mawenlong <mwljndx@163.com>
2019-11-14 19:46:33 +0800

    fix

--- demo/src/main/java/com/mwl/thread/Main2.java
+++ demo/src/main/java/com/mwl/thread/Main2.java
@@ -6,7 +6,7 @@ package com.mwl.thread;
  */
 public class Main2 {
 
-    private static Object lock = new Object();
+    private static final Object lock = new Object();
     private static int count = 0;
 
     public static void main(String[] args) {
