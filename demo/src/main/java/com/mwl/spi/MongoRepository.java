1d4f5957d604e430fc5f327d46b48fc0821f8266
mawenlong <mwljndx@163.com>
2019-11-19 20:08:16 +0800

    intern

--- demo/src/main/java/com/mwl/spi/MongoRepository.java
+++ demo/src/main/java/com/mwl/spi/MongoRepository.java
@@ -7,6 +7,6 @@ package com.mwl.spi;
 public class MongoRepository implements IRepository {
     @Override
     public void save(String data) {
-        System.out.println("Save " + data + " to Mongo");
+        System.out.println("Save " + data + " to Mongo。");
     }
 }
