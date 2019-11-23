8337d34ed283d00593ce139350d66321fd9c73fb
mawenlong <mwljndx@163.com>
2019-11-23 20:34:46 +0800

    fix

--- demo/src/main/java/com/mwl/bytes/Main.java
+++ demo/src/main/java/com/mwl/bytes/Main.java
@@ -10,44 +10,44 @@ public class Main {
     private static ByteBuffer buffer = ByteBuffer.allocate(8);
 
     public static void main(String[] args) {
-        //测试 int 转 byte
+        // 测试 int 转 byte
         int int0 = 234;
         byte byte0 = intToByte(int0);
         System.out.println("byte0=" + byte0);//byte0=-22
-        //测试 byte 转 int
+        // 测试 byte 转 int
         int int1 = byteToInt(byte0);
         System.out.println("int1=" + int1);//int1=234
 
-        //测试 int 转 byte 数组
+        // 测试 int 转 byte 数组
         int int2 = 1417;
         byte[] bytesInt = intToByteArray(int2);
         System.out.println("bytesInt=" + bytesInt);//bytesInt=[B@de6ced
-        //测试 byte 数组转 int
+        // 测试 byte 数组转 int
         int int3 = byteArrayToInt(bytesInt);
         System.out.println("int3=" + int3);//int3=1417
 
 
-        //测试 long 转 byte 数组
+        // 测试 long 转 byte 数组
         long long1 = 2223;
         byte[] bytesLong = longToBytes(long1);
         System.out.println("bytes=" + bytesLong);//bytes=[B@c17164
-        //测试 byte 数组 转 long
+        // 测试 byte 数组 转 long
         long long2 = bytesToLong(bytesLong);
         System.out.println("long2=" + long2);//long2=2223
     }
 
 
-    //byte 与 int 的相互转换
+    // byte 与 int 的相互转换
     public static byte intToByte(int x) {
         return (byte) x;
     }
 
     public static int byteToInt(byte b) {
-        //Java 总是把 byte 当做有符处理；我们可以通过将其和 0xFF 进行二进制与得到它的无符值
+        // Java 总是把byte当做有符处理；可以通过将其和 0xFF 进行二进制与得到它的无符值
         return b & 0xFF;
     }
 
-    //byte 数组与 int 的相互转换
+    // byte 数组与 int 的相互转换
     public static int byteArrayToInt(byte[] b) {
         return b[3] & 0xFF |
                (b[2] & 0xFF) << 8 |
@@ -64,7 +64,7 @@ public class Main {
         };
     }
 
-    //byte 数组与 long 的相互转换
+    // byte 数组与 long 的相互转换
     public static byte[] longToBytes(long x) {
         buffer.putLong(0, x);
         return buffer.array();
