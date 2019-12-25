cc68cd6470b7518e327f2043cea7280c6039dec5
mawenlong <mwljndx@163.com>
2019-12-25 20:37:34 +0800

    fix

--- demo/src/main/java/com/mwl/lambda/DemoClass.java
+++ demo/src/main/java/com/mwl/lambda/DemoClass.java
@@ -22,7 +22,7 @@ public class DemoClass {
 
     public static void main(String[] args) {
         DemoClass demo2 = new DemoClass();
-        // 代码定义上有2个参数, 第一个参数为this
+        // 代码定义上有2个参数, 第一个参数为this。
         // 但实际上调用的时候只需要一个参数
         demo2.normalMethod(1);
     }
