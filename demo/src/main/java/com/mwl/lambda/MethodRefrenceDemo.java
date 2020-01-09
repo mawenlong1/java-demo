e0903ea99a3d5fa0e7ac6ecaa199f2b38be59056
mawenlong <mwljndx@163.com>
2020-01-09 20:32:42 +0800

    fix

--- demo/src/main/java/com/mwl/lambda/MethodRefrenceDemo.java
+++ demo/src/main/java/com/mwl/lambda/MethodRefrenceDemo.java
@@ -20,7 +20,7 @@ public class MethodRefrenceDemo {
         System.out.println(methodRefrence2.applyAsInt(111));
 
         // 对同一个实例方法normalMethod也可以使用静态引用。
-        // 代码上normalMethod虽然只有一个参数,但实际上有一个隐含的this函数。
+        // 代码上normalMethod虽然只有一个参数，但实际上有一个隐含的this函数。
         // 所以使用的是2个参数bifunction函数接口。
         BiFunction<DemoClass, Integer, Integer> methodRefrence3 = DemoClass::normalMethod;
         System.out.println(methodRefrence3.apply(demo, 111));
