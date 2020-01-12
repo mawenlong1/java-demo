c62397e9dd1226951f35fc916570580e1d51e53b
mawenlong <mwljndx@163.com>
2020-01-12 22:03:18 +0800

    fix

--- demo/src/main/java/com/mwl/bytes/Main.java
+++ demo/src/main/java/com/mwl/bytes/Main.java
@@ -62,7 +62,7 @@ public class Main {
         };
     }
 
-    // byte数组与long的相互转换
+    // byte数组与long的相互转换。
     public static byte[] longToBytes(long x) {
         buffer.putLong(0, x);
         return buffer.array();
